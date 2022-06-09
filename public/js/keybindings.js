65001f95eeae071815e8c0b77bcb4bfc952795a9
dgl888 <danielgreylove@gmail.com>
2022-06-09 11:04:27 -0400

    Groundup - Added keybindings for the first paddle.

---
+++ public/js/keybindings.js
@@ -0,0 +1,13 @@
+import { app } from './app';
+
+export let keybindings = function (e) {
+
+    // Player one commands are 'w' to increase height, and 's' to decrease height.
+    if(e.keycode === 87 || e.which == 87) {
+        app.getNode('paddleOne').y += -50
+    }
+    else if(e.keycode === 83 || e.which == 83) {
+        app.getNode('paddleOne').y += 50
+    }
+
+}
