5974679d42447e92ee6c3b7e9dfcb52bd9c4ef90
dgl888 <dglove@liberty.edu>
2022-06-21 20:12:27 -0400

    MyBranch - Fixed the hitbox of the ball again. The right side of the ball was clipping into the right paddle.

--- public/js/collision.js
+++ public/js/collision.js
@@ -5,9 +5,9 @@ export function collisionCheck (paddle) {
     let [ball, paddleOne, paddleTwo, scoreOne, scoreTwo, halfCourt] = getNodes();
 
     ball.topSide = ball.y - ball.r;
-    ball.bottomSide = ball.y + ball.r;
+    ball.bottomSide = ball.y;
     ball.leftSide = ball.x - ball.r;
-    ball.rightSide = ball.x + ball.r;
+    ball.rightSide = ball.x + ball.width;
 
     paddle.topSide = paddle.y;
     paddle.bottomSide = paddle.y + paddle.height;
